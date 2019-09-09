4c2638871d1df29f2e1d76016889aba2bd1f2c9a
U-BSD\411036 <411036@SHS-10DEC4F83.beaverton.k12.or.us>
2019-09-09 09:10:36 -0700

    First rev (incomplete)

---
+++ Palindrome.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+  char str[81];
+  char modStr[81];
+  char modStrR[81];
+  cout << "Enter a string to compare whether or not it is a palindrome" << endl;
+
+  cin.get(str, 81);
+  cin.get();
+
+  int ct = 0;
+  for(int i = strlen(str)-1; i >= 0; i--){
+    
+  }
+  
+  return 0;
+}
